bf890f54a84825249e21c95cf58fcbc6faee63be
xtzws <foxfire_savvier.3i@icloud.com>
2025-05-24 00:58:05 -0400

    Added some basic math that doesn't work (forgot to stage my changes)

--- Calc.swift
+++ Calc.swift
@@ -1,8 +1,29 @@
-//
 //  Calc.swift
 //  bignumcalc
+//  2025-03-31
 //
-//  Created by Isaac Greene on 2025-03-31.
+// The author disclaims copyright to this source code. In place of
+// a legal notice, here is a blessing:
+//
+//    May you do good and not evil.
+//    May you find forgiveness for yourself and forgive others.
+//    May you share freely, never taking more than you give.
 //
 
 import Foundation
+var a: Double
+var b: Double
+var operation: String
+
+print("Enter first number: ")
+a = Double(readLine() ?? "") ?? 0
+
+print("Enter operation (+, -, *, /): ")
+operation = readLine() ?? ""
+
+print("Enter second number: ")
+b = Double(readLine() ?? "") ?? 0
+
+switch operation {
+    
+}
